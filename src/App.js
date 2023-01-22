964ce4c96b985df5cc248ce5b05800efdc7dc6dc
Vyankatesh <vrnyati@gmail.com>
2023-01-23 02:42:53 +0530

    Animation effect for theme and logos

--- src/App.js
+++ src/App.js
@@ -1,39 +1,52 @@
-
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Home from "./Pages/Home";
 
 import { Login } from "./Pages/LoginSignUp/Login";
 import { SignUp } from "./Pages/LoginSignUp/SignUp";
-import { useCookies } from 'react-cookie';
+import { useCookies } from "react-cookie";
+import { useSelector } from "react-redux";
 
 const App = () => {
+  const [animationClasses, setAnimationClasses] = useState("");
+  const themeClasses = `absolute left-0 dark_theme ${animationClasses}`;
+  const dark = useSelector((state) => state.theme.darkTheme)
 
-  const [cookies, setCookie, deleteCookie] = useCookies(['user']);
+  const themeChangeAnimationHandler = () => {
+    if (animationClasses === "" && !dark) {
+      setAnimationClasses("hover bg-black");
+      setTimeout(() => {
+        setAnimationClasses("");
+      }, 500);
+    } else {
+      setAnimationClasses("hover bg-white");
+      setTimeout(() => {
+        setAnimationClasses("");
+      }, 500);
+    }
+  };
+
+  const [cookies, setCookie, deleteCookie] = useCookies(["user"]);
 
   const cookieInitalCheck = () => {
-    if(cookies.username === undefined){
+    if (cookies.username === undefined) {
       // No Username
-    }
-    else{
+    } else {
       // username && get data from backend && set send data to redux
     }
     // console.log(cookies.username)
     // deleteCookie('username')
-  }
-
-
+  };
 
   // useEffect(() => {
   //   cookieInitalCheck()
   // }, [])
 
-
-
   return (
-    <div className="flex flex-col h-screen">
-      <Navbar />
+    <div className="flex flex-col h-screen relative">
+      <div className={themeClasses}></div>
+      <Navbar themeChangeAnimationHandler={themeChangeAnimationHandler} />
       <Routes>
         <Route path="/" element={<Navigate replace to="/home" />} />
         <Route path="/login" element={<Login />} />
