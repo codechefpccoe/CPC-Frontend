572e73e0c950c1c6e60debb9acf2960f298427ea
vyankatesh <vrnyati@gmail.com>
2023-01-28 14:57:47 -0800

    responsive home page

--- src/Pages/Home/Content.jsx
+++ src/Pages/Home/Content.jsx
@@ -28,8 +28,8 @@ const Content = () => {
   }, [index]);
 
   return (
-    <div className="w-1/2 justify-center flex flex-col gap-6 px-8 dark:text-white z-[100]">
-      <div className="font-iceberg text-4xl">
+    <div className="w-1/2 justify-center flex flex-col gap-6 px-8 dark:text-white z-[100] max-lg:px-4 max-lg:gap-3 max-md:w-3/4 max-md:text-center max-vs:w-[90%]">
+      <div className="font-iceberg text-4xl max-lg:text-3xl max-vs:text-2xl">
         <h1>
           <span className="text-primary">CODE</span> CHEF
         </h1>
@@ -37,13 +37,13 @@ const Content = () => {
           PCCOE <span className="text-primary">CHAPTER</span>
         </h1>
       </div>
-      <div className="font-cairo text-base leading-6 pr-8">
+      <div className="font-cairo text-base leading-6 pr-8 text-justify max-lg:pr-6 max-md:pr-0 max-vs:text-sm">
         <p className={contentAnimationClasss} ref={contentRef}></p>
       </div>
-      <div className="font-cairo text-primary text-2xl">
+      <div className="font-cairo text-primary text-2xl max-lg:text-xl max-vs:text-base">
         <h1>Let Us Help You To get Started!</h1>
       </div>
-      <div className="flex gap-8 text-3xl">
+      <div className="flex gap-8 text-3xl max-lg:text-2xl max-lg:gap-6 max-md:justify-center max-vs:text-xl">
         <ImLinkedin className="text-[#0A66C2] cursor-pointer" />
         <ImFacebook2 className="text-[#395185] cursor-pointer" />
         <FiInstagram className="cursor-pointer" />
