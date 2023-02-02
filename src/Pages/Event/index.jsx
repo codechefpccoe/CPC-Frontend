913d50f58c94e719152ff7814c6bdd067c2f5585
Ajinkyap331 <ajinkyap331@gmail.com>
2023-02-02 21:08:23 +0530

    Nothing

--- src/Pages/Event/index.jsx
+++ src/Pages/Event/index.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import { UpcomingEvent } from "./UpcomingEvent";
 import PastEvent from "./PastEvent";
+import Navbar from "../../Components/Navbar";
 
 export const Event = () => {
   return (
+    <>
+    <Navbar />  
     <div className="pt-5 text-center">
       <h1 className="font-cairo font-bold">Upcoming Event</h1>
       <div className="flex justify-center">
         <UpcomingEvent />
       </div>
       <h1 className="mt-2 font-cairo font-bold">Past Event</h1>
-      <div className="flex justify-center">
+      <div className="">
         <PastEvent />
       </div>
     </div>
+    </>
+    
   );
 };
