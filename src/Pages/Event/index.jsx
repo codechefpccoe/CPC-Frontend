88d27490c8d136044edb52051f361ec340a2a9b8
vyankatesh <vrnyati@gmail.com>
2023-02-08 19:59:55 +0530

    working: event details pop up window

--- src/Pages/Event/index.jsx
+++ src/Pages/Event/index.jsx
@@ -1,13 +1,14 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { UpcomingEvent } from "./UpcomingEvent";
 import PastEvent from "./PastEvent";
 import Navbar from "../../Components/Navbar";
 import { db } from "../../Config/Firebase";
+import EventDetails from "./EventDetails";
 
 export const Event = () => {
   const [EventData, setud] = useState([]);
+  const [detailsHide, setDetailsHide] = useState("hidden");
 
- 
   useEffect(() => {
     db.collection("events")
       .get()
@@ -16,8 +17,17 @@ export const Event = () => {
         setud(data);
       });
   }, []);
+
+  const detailsHideChnageHandler = () => {
+    if (detailsHide === "hidden") {
+      setDetailsHide("");
+    } else {
+      setDetailsHide("hidden");
+    }
+  };
+
   return (
-    <>
+    <div className="h-screen flex flex-col gap-10 relative">
       <Navbar />
       <div className="pt-5 text-center">
         <h1 className="font-cairo font-bold">Upcoming Event</h1>
@@ -26,9 +36,14 @@ export const Event = () => {
         </div>
         <h1 className="mt-2 font-cairo font-bold">Past Event</h1>
         <div className="">
-          <PastEvent EventData={EventData} />
+          <PastEvent EventData={EventData} detailsHideChnageHandler={detailsHideChnageHandler} />
         </div>
       </div>
-    </>
+      <div
+        className={`absolute bottom-0 right-0 w-[30%] bg-white h-[86%] shadow-2xl drop-shadow-lg ${detailsHide}`}
+      >
+        <EventDetails detailsHideChnageHandler={detailsHideChnageHandler} />
+      </div>
+    </div>
   );
 };
